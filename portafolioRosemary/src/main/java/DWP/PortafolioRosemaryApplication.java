f1b73fd90ec5daa53eba6765f114d4845beb54f7
Rossemary Carballo <rcarballo70861@ufide.ac.cr>
2025-01-22 20:57:17 -0600

    semana 2

---
+++ portafolioRosemary/src/main/java/DWP/PortafolioRosemaryApplication.java
@@ -0,0 +1,13 @@
+package DWP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioRosemaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioRosemaryApplication.class, args);
+	}
+
+}
