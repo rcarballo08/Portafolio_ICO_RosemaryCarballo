82ecee3ab91699fa7116ab76429799b37f25c0fb
Rossemary Carballo <rcarballo70861@ufide.ac.cr>
2025-02-23 21:15:23 -0600

    Semana 6
    codigo que vimos en clase y codigo renovado

---
+++ portafolioRosemary/src/main/java/DWP/DWP/service/CategoriaService.java
@@ -0,0 +1,11 @@
+package DWP.DWP.service;
+
+import DWP.DWP.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
